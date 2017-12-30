4d9d9748ec2d6cd5244cb9ad07f3227bca7b2f5e
k.siu92@gmail.com <k.siu92@gmail.com>
2017-12-30 12:02:59 -0500

    Phase 4 of Part 2 Complete - added bench backend. Already started on bench redux cycle.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Bench.delete_all
+User.delete_all
+
+Bench.create!(
+  description: "It's always clean!",
+  lat: 37.754631,
+  lng: -122.444132
+)
+
+Bench.create!(
+  description: "I hate it. There's always poop on the seat.",
+  lat: 37.757956,
+  lng: -122.441707
+)
+
+Bench.create!(
+  description: "Great bench with a lovely view.",
+  lat: 37.765202,
+  lng: -122.439910
+)
